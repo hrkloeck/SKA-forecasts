079d1fb152ebf0076c11b5eb5885ea46645551e4
HRK <hkloeckner@dhcp2511.mpifr-bonn.mpg.de>
2025-11-06 15:15:56 +0100

    figured it out stuff

---
+++ HI_galaxy_lib.py
@@ -0,0 +1,112 @@
+
+import numpy as np
+
+def Simpson(ff, a, b, N=100):
+    '''
+    Returns the integral of a function between a and b
+
+    ff = function to be integrated
+    a, b = limits of integration
+    N = number of steps
+    '''
+    
+    h = (b-a)/N
+    x = np.linspace(a,b,N+1)
+    y = ff(x)
+    I = h/3 * (y[0] + y[-1] + 4*np.sum(y[1:N:2]) + 2*np.sum(y[2:N-1:2]))
+    return I
+
+def N_func(z, S_rms, S_area):
+    '''
+    Returns the number of detected HI sources and the lower boundary of the emission line amplitude [𝜇Jy]
+
+    z = observed redshift
+    S_rms = array's noise [𝜇Jy]
+    t_obs = observation time (per pointing) [s]
+    N_ant = total number of antennas
+    S_area = observed survey area [sq deg]
+    '''
+    
+    if S_rms < 1.:
+        c1 = 6.21
+        c2 = 1.72
+        c3 = 0.79
+        A  = .1
+
+    elif 1. <= S_rms < 3.:
+        c1 = 6.55
+        c2 = 2.02
+        c3 = 3.81
+        A  = 1.
+
+    elif 3. <= S_rms < 5.:
+        c1 = 6.53
+        c2 = 1.93
+        c3 = 6.22
+        A  = 3.
+
+    elif 5. <= S_rms < 6.:
+        c1 = 6.55
+        c2 = 1.93
+        c3 = 6.22
+        A  = 5.
+
+    elif 6. <= S_rms < 7.3:
+        c1 = 6.58
+        c2 = 1.95
+        c3 = 6.69
+        A  = 6.
+    
+    elif 7.3 <= S_rms < 10:
+        c1 = 6.55
+        c2 = 1.92
+        c3 = 7.08
+        A  = 7.3
+
+    elif 10 <= S_rms < 23:
+        c1 = 6.44
+        c2 = 1.83
+        c3 = 7.59
+        A  = 10
+
+    elif 23 <= S_rms < 40:
+        c1 = 6.02
+        c2 = 1.43
+        c3 = 9.03
+        A  = 23
+
+    elif 40 <= S_rms < 70:
+        c1 = 5.74
+        c2 = 1.22
+        c3 = 10.58
+        A  = 40
+
+    elif 70 <= S_rms < 150:
+        c1 = 5.63
+        c2 = 1.41
+        c3 = 15.49
+        A  = 70
+
+    elif 150 <= S_rms < 200:
+        c1 = 5.48
+        c2 = 1.33
+        c3 = 16.62
+        A  = 150
+
+    elif 200 <= S_rms < 900:
+        c1 = 5.00
+        c2 = 1.04
+        c3 = 17.52
+        A  = 200
+
+    else: # in case there is too much noise, we consider only 5 sources will be detected
+        return 5, 900
+
+    dN_dz = lambda z: 10**c1 * z**c2 * np.exp(-c3*z)
+    a, b = z-.1, z+.1
+    N = Simpson(dN_dz,a,b) * S_area # integration over a bin of 0.2 centered in z
+
+    #print('Simpson ',Simpson(dN_dz,a,b),'N ',np.array(N).astype(int), A,a,b)
+
+    
+    return np.array(N).astype(int), A
---
+++ Redshift_drift_machine.py
@@ -0,0 +1,115 @@
+
+#
+# This is based on the stuff from Carlos and Livia
+#
+#
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+np.set_printoptions(linewidth=np.inf)
+
+import results_plotting_lib as dvplt
+import redshiftdrift_lib as dvlib
+import Fisher_matrix_lib as fmlib
+
+p_LCDM  = np.array([70., -.55, 1.]) # H0, q0, j0
+
+#
+#  Here start the playing around
+#
+
+fwhm_def   = 150E5                       # cm/s (see Obreschkow 2009)
+z_val      = [0.1, 0.2, 0.3, 0.4, 0.5]   # redshifts
+z_eg       = 0.25                        # example redshift
+t_obs_def  = 3600*1                      # s
+Dnu_val    = [1E-3,1E-1,1E0]             # [0.1.285e3, 1e4]              # Hz
+S_area_val = [5000,10000,30000]         # sq deg
+
+t_exp_def  = 12                          # yr
+N_ant_def  = 144                         # integer
+
+
+# === Plot number counts ===
+#
+# this should be flat and should not change since we use the channel width
+#
+# dvplt.plot_Nz(z_val, Dnu_val, S_area_val, t_obs_def, N_ant_def)
+
+
+# === Plot colour image of error estimates (image dimension sky area (horizontal) and channel width (vertical)) ===
+#
+#dvplt.im_sigmav(z_eg, Dnu_val, S_area_val, t_obs_def, N_ant_def, fwhm_def,doprtinfo=False,doplot=True)
+
+# === Plot colour image of significance of error estimates versus theoretical model (image dimension sky area (horizontal) and channel width (vertical)) ===
+#
+#dvplt.im_vsignificance(z_eg, Dnu_val[0], Dnu_val[-1], S_area_val[0], S_area_val[-1], t_exp_def, p_LCDM, t_obs_def, N_ant_def, fwhm_def, N=100)
+
+
+# === Plot diagram error estimates versus redshift per sky area  ===
+#
+#dvplt.plot_sigmav(z_val, Dnu_val, S_area_val, t_obs_def, N_ant_def, fwhm_def)
+
+# === Plot diagram significane of error estimates/theoretical model versus redshift per sky area  ===
+#
+# dvplt.plot_vsignificance(z_val, Dnu_val, S_area_val, t_exp_def, p_LCDM, t_obs_def, N_ant_def, fwhm_def)
+
+
+def analysis_FoM(z, t_obs, t_exp, N_ant, Dnu, S_area, fwhm):
+    
+    sigma_v = dvlib.sigma_v_func(z, t_obs, N_ant, Dnu, S_area, fwhm)
+    delta_v = lambda p: dvlib.delta_v_func(z, p, t_exp)
+
+    F = fmlib.Fisher_matrix(p_LCDM, delta_v, sigma_v)
+    
+    return fmlib.FoM(F, 1, 2), fmlib.unc(F) # Figure of Merit between q0 and j0 and uncertainties
+
+
+def hrk_analysis_results(z, t_obs, t_exp, N_ant, Dnu, S_area, fwhm): # s, yrs, integer
+
+    FMANA = analysis_FoM(z, t_obs, t_exp, N_ant, Dnu, S_area, fwhm)
+    FoM   = FMANA[0]
+    unc   = FMANA[1]
+
+    delta_v = dvlib.delta_v_func(z, p_LCDM, t_exp)
+    sigma_v = dvlib.sigma_v_func(z, t_obs, N_ant, Dnu, S_area, fwhm)
+    
+    return f'\n=== Analysis ===\nRedshift: {z}\nDnu: {Dnu}\nS_area: {S_area}\nExpected drift: {delta_v}\nMeasured Error: {sigma_v}\nFigure of Merit: {FoM} \nUncertainties of H0, q0, j0: {unc}'
+
+
+def analysis(t_obs, t_exp, N_ant, Dnu, S_area, fwhm): # s, yrs, integer
+    z1 = np.array([.3])
+    z2 = np.array([.3, .5])
+    z3 = np.array([.1, .3, .5])
+
+
+    
+    sys.exit(-1)
+    FoM = np.array([analysis_FoM(z1, t_obs, t_exp, N_ant, Dnu, S_area, fwhm)[0],
+                    analysis_FoM(z2, t_obs/2, t_exp, N_ant, Dnu, S_area, fwhm)[0],
+                    analysis_FoM(z3, t_obs/3, t_exp, N_ant, Dnu, S_area, fwhm)[0]])
+
+    unc = np.array([analysis_FoM(z1, t_obs, t_exp, N_ant, Dnu, S_area, fwhm)[1],
+                    analysis_FoM(z2, t_obs/2, t_exp, N_ant, Dnu, S_area, fwhm)[1],
+                    analysis_FoM(z3, t_obs/3, t_exp, N_ant, Dnu, S_area, fwhm)[1]])
+    i   = np.argmax(FoM)
+
+    if i == 0:
+        return f'Array of redshifts: {z1}\nFigure of Merit: {FoM[0]} \nUncertainties of H0, q0, j0: {unc[0]}'
+    if i == 1:
+        return f'Array of redshifts: {z2}\nFigure of Merit: {FoM[1]} \nUncertainties of H0, q0, j0: {unc[1]}'
+    if i == 2:
+        return f'Array of redshifts: {z3}\nFigure of Merit: {FoM[2]} \nUncertainties of H0, q0, j0: {unc[2]}'
+
+
+#
+# === First attemp to do the analyis ===
+#
+
+best_values = dvplt.im_sigmav(z_eg, Dnu_val, S_area_val, t_obs_def, N_ant_def, fwhm_def,doprtinfo=True,doplot=False)
+
+best_dnu    = best_values[0][0]
+best_area   = best_values[0][1]
+
+print(hrk_analysis_results(z_eg,t_obs_def, t_exp_def, N_ant_def, best_dnu, best_area, fwhm_def))
+
