6c15d75c2bb0489b3e8d298fb860e084409b72f3
Lívia Oliveira <liviagiardinitavares@gmail.com>
2025-11-13 13:04:08 +0000

    bold ellps labels + print analysis

--- results_plotting_lib.py
+++ results_plotting_lib.py
@@ -262,6 +262,7 @@ def plot_ellipses(z_val, Dnu_val, S_area_val, t_obs, t_exp, N_ant, fwhm, p, prio
             delta_v = lambda p: delta_v_func(z_val, p, t_exp)
             F = Fisher_matrix(p, delta_v, sigma_v, priors)
             fom = FoM(F, 1, 2)
+            #print(Dnu, S_area, unc(F))
             draw_ellipse(F, 1, 2, delta_chi2=2.3, center=(p[1], p[2]), ax=ax[i,j], label=r'1$\sigma$')
             draw_ellipse(F, 1, 2, delta_chi2=6.17, center=(p[1], p[2]), ax=ax[i,j], label=r'2$\sigma$')
             draw_ellipse(F, 1, 2, delta_chi2=11.8, center=(p[1], p[2]), ax=ax[i,j], label=r'3$\sigma$')
@@ -269,11 +270,11 @@ def plot_ellipses(z_val, Dnu_val, S_area_val, t_obs, t_exp, N_ant, fwhm, p, prio
             ax[i,j].text(-8, -40, f'FoM = {fom:.2f}')
 
     for i, a in enumerate(ax[:, 0]):
-        a.set_ylabel(f"$j_0$\n[Dnu = {Dnu_val[i]} Hz]", fontsize=12)
+        a.set_ylabel(f"$\\mathbf{{j_0}}$\n[Dnu = {Dnu_val[i]} Hz]", fontsize=12)
     for j, a in enumerate(ax[0]):
         a.set_title(f"S_area = {S_area_val[j]} sq deg")
     for j, a in enumerate(ax[-1]):
-        a.set_xlabel(f"$q_0$")
+        a.set_xlabel(f"$\\mathbf{{q_0}}$")
 
     fig.tight_layout()
     if savefig:
